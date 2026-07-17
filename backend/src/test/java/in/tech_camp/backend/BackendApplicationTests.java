d4b85c58b300734532dc3c8a90d32782541db2f5
sho <sho.haraguchi@infomart.co.jp>
2026-07-17 10:29:27 +0900

    Initial commit: Add backend and front

---
+++ backend/src/test/java/in/tech_camp/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_camp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
