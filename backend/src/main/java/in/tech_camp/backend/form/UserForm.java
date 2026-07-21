2ddd18ae395221080fbf27218606c88d2ca3fcd5
kaipupupuland678 <k.hiranuma@infomart.co.jp>
2026-07-21 13:26:58 +0900

    UserFormの実装

--- backend/src/main/java/in/tech_camp/backend/form/UserForm.java
+++ backend/src/main/java/in/tech_camp/backend/form/UserForm.java
@@ -1,5 +1,33 @@
 package in.tech_camp.backend.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class UserForm {
-  
+
+    @NotBlank(message = "ユーザー名は必須です")
+    private String name;
+
+    @NotBlank(message = "メールアドレスは必須です")
+    @Email(message = "有効なメールアドレス形式で入力してください")
+    private String email;
+
+    @NotBlank(message = "パスワードは必須です")
+    @Size(min = 6, message = "パスワードは６文字以上で入力してください")
+    private String password;
+
+    @NotBlank(message = "確認用パスワードは必須です")
+    private String passwordConfirmation;
+
+    @NotBlank(message = "プロフィールは必須です")
+    private String profile;
+
+    @NotBlank(message = "所属は必須です")
+    private String affiliation;
+
+    @NotBlank(message = "役職は必須です")
+    private String position;
 }
