6666e0f05c5bae453f4203c56932ff7461e2d835
gaozhao27 <z.gao@infomart.co.jp>
2026-07-21 16:54:12 +0900

    CORS設定の修正

--- backend/src/main/java/in/tech_camp/backend/controller/PrototypeController.java
+++ backend/src/main/java/in/tech_camp/backend/controller/PrototypeController.java
@@ -7,11 +7,9 @@ import in.tech_camp.backend.service.PrototypeService;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class PrototypeController {
   private final PrototypeService prototypeService;
   public PrototypeController(PrototypeService prototypeService) {
