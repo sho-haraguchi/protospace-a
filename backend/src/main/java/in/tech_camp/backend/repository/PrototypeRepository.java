14fc16b241eeb3475400bd1f37d19c3e63e4b905
sho <sho.haraguchi@infomart.co.jp>
2026-07-22 09:21:51 +0900

    プロトタイプ詳細表示

--- backend/src/main/java/in/tech_camp/backend/repository/PrototypeRepository.java
+++ backend/src/main/java/in/tech_camp/backend/repository/PrototypeRepository.java
@@ -1,5 +1,17 @@
 package in.tech_camp.backend.repository;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import in.tech_camp.backend.entity.PrototypeEntity;
+
+@Mapper
 public interface PrototypeRepository {
-  
+
+  // プロトタイプ詳細画面表示
+  @Select("SELECT p.*, u.name AS \"user.name\" " +
+            "FROM prototypes p " +
+            "LEFT JOIN users u ON p.user_id = u.id " +
+            "WHERE p.id = #{id}")
+    PrototypeEntity findById(Integer id);
 }
