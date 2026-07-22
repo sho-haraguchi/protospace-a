001227ba49a2aa2cb3d56a1f0a159cbbca3a6e21
gaozhao27 <z.gao@infomart.co.jp>
2026-07-22 14:03:27 +0900

    プロトタイプフォーム作成

--- backend/src/main/java/in/tech_camp/backend/form/PrototypeForm.java
+++ backend/src/main/java/in/tech_camp/backend/form/PrototypeForm.java
@@ -1,5 +1,21 @@
 package in.tech_camp.backend.form;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class PrototypeForm {
+  @NotBlank(message = "プロトタイプの名称を入力してください")
+  private String name;
+
+  @NotBlank(message = "キャッチコピーを入力してください")
+  private String slogan;
+
+  @NotBlank(message = "コンセプトを入力してください")
+  private String concept;
+  
+  private MultipartFile image;
   
 }
