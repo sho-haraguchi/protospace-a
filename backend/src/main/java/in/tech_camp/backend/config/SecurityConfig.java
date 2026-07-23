999ce7ae8c681f657f0718daea7c1aef03c9292d
kaipupupuland678 <k.hiranuma@infomart.co.jp>
2026-07-23 15:13:40 +0900

    Configの追加

--- backend/src/main/java/in/tech_camp/backend/config/SecurityConfig.java
+++ backend/src/main/java/in/tech_camp/backend/config/SecurityConfig.java
@@ -10,9 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -34,12 +31,14 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/error").permitAll()
                         .requestMatchers("/css/**", "/images/**", "/").permitAll()
-                        
+                        .requestMatchers("/uploads/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users").permitAll()  
                         .requestMatchers(HttpMethod.POST, "/api/users/login").permitAll() 
                         .requestMatchers(HttpMethod.GET, "/api/users/me").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/users/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users/logout").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/prototypes/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/prototypes/**").permitAll()
 
                         .anyRequest().authenticated());
 
