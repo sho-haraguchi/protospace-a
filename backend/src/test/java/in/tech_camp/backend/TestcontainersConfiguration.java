8158996c4c8f1a207f3db781b1fe0c7e182717dc
sho <sho.haraguchi@infomart.co.jp>
2026-07-27 15:36:25 +0900

    重複した記述を削除

--- backend/src/test/java/in/tech_camp/backend/TestcontainersConfiguration.java
+++ backend/src/test/java/in/tech_camp/backend/TestcontainersConfiguration.java
@@ -1,8 +1,5 @@
 package in.tech_camp.backend;
 
-import javax.sql.DataSource;
-
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -19,15 +16,5 @@ public class TestcontainersConfiguration {
                 .withDatabaseName("proto_db_test")
                 .withUsername("postgres")
                 .withPassword("postgres");
-    }
-
-    // テスト環境用の Flyway 設定
-    @Bean(initMethod = "migrate")
-    public Flyway flyway(DataSource dataSource) {
-        return Flyway.configure()
-                .dataSource(dataSource)
-                .locations("classpath:db/migration")
-                .baselineOnMigrate(true)
-                .load();
     }
 }
