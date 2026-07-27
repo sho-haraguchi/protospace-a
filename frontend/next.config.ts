8000f70880a727e3e86fb0cdfcc41c75db42f0ed
sho <sho.haraguchi@infomart.co.jp>
2026-07-27 15:24:51 +0900

    test

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,22 +1,38 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+
   images: {
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
-        port: '8080', // SpringBootのポート番号
+        port: '8080',
+        pathname: '/api/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'protospace-backend.onrender.com',
         pathname: '/api/images/**',
       },
     ],
   },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://protospace-backend.onrender.com/api/:path*',
+      },
+    ];
+  },
+
   async redirects() {
     return [
       {
-        source: '/prototypes/:id/delete', // /prototypes/10/delete へのアクセスを検知
-        destination: '/',                  // トップページへ転送
+        source: '/prototypes/:id/delete',
+        destination: '/',
         permanent: false,
       },
     ];
