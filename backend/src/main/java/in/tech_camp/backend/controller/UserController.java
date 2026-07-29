f55c2c70f28d1423d597f7356d2062ad4f15d36d
gaozhao27 <z.gao@infomart.co.jp>
2026-07-29 16:34:20 +0900

    }括弧の修正

--- backend/src/main/java/in/tech_camp/backend/controller/UserController.java
+++ backend/src/main/java/in/tech_camp/backend/controller/UserController.java
@@ -188,7 +188,7 @@ public class UserController {
         // 4. セッションへ Context を明示的に紐付け
         session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, securityContext);
         }
-    }
+    
 
     /**
      * ユーザー情報更新処理（updateUser）
@@ -225,6 +225,7 @@ public class UserController {
             error.put("message", "更新処理に失敗しました。");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
         }
+     }
     }
    /**
    * ログイン画面表示（showLogin）
@@ -241,4 +242,3 @@ public class UserController {
   /**
    * ユーザー詳細ページ表示（showMypage）
    */
-}
