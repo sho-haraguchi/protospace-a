b9cc52fec465092c76ca000cd05015da0e4d6348
kaipupupuland678 <k.hiranuma@infomart.co.jp>
2026-08-07 11:38:10 +0900

    Add all project files

---
+++ furima-a/src/main/java/in/tech_camp/furima_a/FurimaAApplication.java
@@ -0,0 +1,13 @@
+package in.tech_camp.furima_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurimaAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurimaAApplication.class, args);
+	}
+
+}
---
+++ furima-a/src/test/java/in/tech_camp/furima_a/FurimaAApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_camp.furima_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurimaAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
